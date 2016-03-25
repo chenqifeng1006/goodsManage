ca5d532c124e22066d89f07e7f400cfdcd629c8d
chenqf <qifeng.chen1006@gmail.com>
2016-03-25 23:32:33 +0800

    sdsdsd

--- src/com/goodsmanage/action/BorrowGoodsAction.java
+++ src/com/goodsmanage/action/BorrowGoodsAction.java
@@ -6,13 +6,166 @@ package com.goodsmanage.action;
 
  
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.goodsmanage.dao.BorrowGoodsDAO;
+import com.goodsmanage.dao.GiveGoodsDAO;
 import com.goodsmanage.dao.UserDAO;
+import com.goodsmanage.domain.BorrowGoods;
+import com.goodsmanage.domain.GiveGoods;
 import com.goodsmanage.domain.User;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class BorrowGoodsAction extends ActionSupport {
- 
-	
-	
+	 
+
+
+		private static final long serialVersionUID = 1L;
+		private BorrowGoods entity;
+
+	    
+	  
+		public BorrowGoods getEntity() {
+			return entity;
+		}
+		public void setEntity(BorrowGoods entity) {
+			this.entity = entity;
+		}
+	    private int id;
+
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		/*当前第几页*/
+	    private int currentPage;
+	    public void setCurrentPage(int currentPage) {
+	        this.currentPage = currentPage;
+	    }
+	    public int getCurrentPage() {
+	        return currentPage;
+	    }
+
+	    /*一共多少页*/
+	    private int totalPage;
+	    public void setTotalPage(int totalPage) {
+	        this.totalPage = totalPage;
+	    }
+	    public int getTotalPage() {
+	        return totalPage;
+	    }
+
+
+	    /*当前查询的总记录数目*/
+	    private int recordNumber;
+	    public void setRecordNumber(int recordNumber) {
+	        this.recordNumber = recordNumber;
+	    }
+	    public int getRecordNumber() {
+	        return recordNumber;
+	    }
+
+	    /*业务层对象*/
+	    BorrowGoodsDAO dao = new BorrowGoodsDAO();
+
+		
+
+		/*添加InputCashTable信息*/
+	    @SuppressWarnings("deprecation")
+	    public String add() {
+	        ActionContext ctx = ActionContext.getContext();
+	        
+	        try {
+	           
+	        	BorrowGoodsDAO dao=new BorrowGoodsDAO();
+
+	            dao.add(entity);
+	            ctx.put("message",  java.net.URLEncoder.encode("添加成功!"));
+	            return "success";
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            ctx.put("error",  java.net.URLEncoder.encode("添加失败!"));
+	            return "error";
+	        }
+
+	    
+	    }
+	    
+	    
+	    public String update() {
+	        ActionContext ctx = ActionContext.getContext();
+	        /*根据主键inputId获取InputCashTable对象*/
+	        BorrowGoods goods =dao.getById(id);
+
+	        ctx.put("entity",  goods);
+	        return "modify_view";
+	    }
+	    @SuppressWarnings("deprecation")
+	    public String save() {
+	        ActionContext ctx = ActionContext.getContext();
+	        
+	        try {
+	        	BorrowGoodsDAO goodsDao=new BorrowGoodsDAO();
+	            goodsDao.update(entity);
+	            ctx.put("message",  java.net.URLEncoder.encode("保存成功!"));
+	            return "success";
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            ctx.put("error",  java.net.URLEncoder.encode("保存失败!"));
+	            return "error";
+	        }
+
+	    }
+
+	    /*管理员查询信息*/
+	    public String query() {
+	        if(currentPage == 0) currentPage = 1;
+	        List<BorrowGoods> list = dao.query(currentPage,5,"");
+	        /*计算总的页数和总的记录数*/
+	        dao.CalculateTotalPageAndRecordNumber("");
+	        /*获取到总的页码数目*/
+	        totalPage = dao.getTotalPage();
+	        /*当前查询条件下总记录数*/
+	        recordNumber = dao.getRecordNumber();
+	        ActionContext ctx = ActionContext.getContext();
+	        ctx.put("list",  list);
+	        ctx.put("totalPage", totalPage);
+	        ctx.put("recordNumber", recordNumber);
+	        ctx.put("currentPage", currentPage);
+	        return "query_view";
+	    }
+
+	    
+
+	   
+
+	    /*删除InputCashTable信息*/
+	    public String delete() {
+	        ActionContext ctx = ActionContext.getContext();
+	        try { 
+	        	dao.delete(id);
+	            ctx.put("message",  java.net.URLEncoder.encode("删除成功!"));
+	            return "success";
+	        } catch (Exception e) { 
+	            e.printStackTrace();
+	            ctx.put("error",  java.net.URLEncoder.encode("删除失败!"));
+	            return "error";
+	        }
+	    }
+
+
+		
 }
---
+++ src/com/goodsmanage/dao/BorrowGoodsDAO.java
@@ -0,0 +1,159 @@
+package com.goodsmanage.dao;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.xwork.StringUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.goodsmanage.domain.Admin;
+import com.goodsmanage.domain.BorrowGoods;
+import com.goodsmanage.domain.GiveGoods;
+import com.goodsmanage.domain.User;
+import com.goodsmanage.utils.HibernateUtil;
+
+
+public class BorrowGoodsDAO {
+
+    /*每页显示记录数目*/
+    private final int PAGE_SIZE = 5;
+
+    /*保存查询后总的页数*/
+    private int totalPage;
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    /*保存查询到的总记录数*/
+    private int recordNumber;
+    public void setRecordNumber(int recordNumber) {
+        this.recordNumber = recordNumber;
+    }
+    public int getRecordNumber() {
+        return recordNumber;
+    }
+
+    /*添加图书信息*/
+    public void add(BorrowGoods entity) throws Exception {
+        Session s = null;
+        Transaction tx = null;
+        try { 
+            s = HibernateUtil.getSession();
+            tx = s.beginTransaction(); 
+            s.save(entity);
+            tx.commit();
+        } catch (HibernateException e) {
+            if(tx != null)
+                tx.rollback();
+            throw e;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    /*查询信息*/
+    public ArrayList<BorrowGoods> query(int currentPage,int pageSize,String goodsname) { 
+        Session s = null; 
+        try {
+            s = HibernateUtil.getSession();
+            String hql = "From BorrowGoods borrowGoods where 1=1";
+            if(StringUtils.isNotBlank(goodsname)){
+            	hql+="and goodsname="+goodsname;
+            }
+/*            if(null != member && !member.getUserNo().equals("")) hql += " and inputCashTable.member.userNo='" + member.getUserNo() + "'";
+            if(null != inputFroms && inputFroms.getInputClassId()!=0) hql += " and inputCashTable.inputFroms.inputClassId=" + inputFroms.getInputClassId();
+            if(null != payWayObj && payWayObj.getPayWayId()!=0) hql += " and inputCashTable.payWayObj.payWayId=" + payWayObj.getPayWayId();
+            if(!inputDateTime.equals("")) hql = hql + " and inputCashTable.inputDateTime like '%" + inputDateTime + "%'";*/
+            Query q = s.createQuery(hql);
+            /*计算当前显示页码的开始记录*/
+            int startIndex = (currentPage-1) * pageSize;
+            q.setFirstResult(startIndex);
+            q.setMaxResults(this.PAGE_SIZE);
+            List list = q.list();
+            return (ArrayList<BorrowGoods>) list;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    /*计算总的页数和记录数*/
+    public void CalculateTotalPageAndRecordNumber(String goodsname) {
+        Session s = null;
+        try {
+            s = HibernateUtil.getSession();
+            String hql = "From BorrowGoods borrowGoods where 1=1";
+            if(StringUtils.isNotBlank(goodsname)){
+            	hql+="and goodsname="+goodsname;
+            }
+     /*       if(null != member && !member.getUserNo().equals("")) hql += " and inputCashTable.member.userNo='" + member.getUserNo() + "'";
+            if(null != inputFroms && inputFroms.getInputClassId()!=0) hql += " and inputCashTable.inputFroms.inputClassId=" + inputFroms.getInputClassId();
+            if(null != payWayObj && payWayObj.getPayWayId()!=0) hql += " and inputCashTable.payWayObj.payWayId=" + payWayObj.getPayWayId();
+            if(!inputDateTime.equals("")) hql = hql + " and inputCashTable.inputDateTime like '%" + inputDateTime + "%'";*/
+            Query q = s.createQuery(hql);
+            List list = q.list();
+            recordNumber = list.size();
+            int mod = recordNumber % this.PAGE_SIZE;
+            totalPage = recordNumber / this.PAGE_SIZE;
+            if(mod != 0) totalPage++;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    /*根据主键获取对象*/
+    public BorrowGoods getById(int id) {
+        Session s = null;
+        try {
+            s = HibernateUtil.getSession();
+            BorrowGoods entity = (BorrowGoods)s.get(BorrowGoods.class, id);
+            return entity;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    /*更新InputCashTable信息*/
+    public void update(BorrowGoods entity) throws Exception {
+        Session s = null;
+        Transaction tx = null;
+        try {
+            s = HibernateUtil.getSession();
+            tx = s.beginTransaction();
+            s.update(entity);
+            tx.commit();
+        } catch (HibernateException e) {
+            if(tx != null)
+            	  tx.rollback();
+            throw e;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+    /*删除信息*/
+    public void delete (int inputId) throws Exception {
+        Session s = null;
+        Transaction tx = null;
+        try {
+            s = HibernateUtil.getSession();
+            tx = s.beginTransaction();
+            Object obj = s.get(BorrowGoods.class, inputId);
+            s.delete(obj);
+            tx.commit();
+        } catch (HibernateException e) {
+            if(tx != null)
+            	  tx.rollback();
+            throw e;
+        } finally {
+            HibernateUtil.closeSession();
+        }
+    }
+
+}
